ff26bd768f6f2a89cea9e0ebacc3e603cc1ec26f
alicegingras <alicegingras@hotmail.com>
2024-04-15 02:07:01 -0400

    les fichiers css

--- db_scripts/routinesRequetes.sql
+++ db_scripts/routinesRequetes.sql
@@ -16,14 +16,14 @@ BEGIN
 SELECT * FROM series WHERE nomSerie LIKE nomSerie2 AND genre LIKE nomGenre AND sousGenre LIKE nomSousGenre AND annee LIKE annee2 AND acteurs LIKE acteur AND nbVotes >= nbVoteMin AND noteGlobale >= noteMin AND saison LIKE saison2 ORDER BY noteGlobale DESC;
 END //
 DELIMITER ;
---procédure permettant de filtrer les films d'un utilisateur spécifique en fonction de plusieurs critères :
+--procédure permettant de filtrer les films dun utilisateur spécifique en fonction de plusieurs critères :
 DELIMITER //
 CREATE PROCEDURE filtrerFilmsUtilisateur (IN nomFilm2 varchar(100), IN nomGenre varchar(50), IN nomSousGenre varchar(50), IN annee2 varchar(6), IN acteur varchar(100),IN nbVoteMin integer, IN noteMin double, IN nomUser varchar(50))
 BEGIN
 SELECT F.*, V.nomUtilisateur, V.note  FROM films F, votesFilms V WHERE F.nomFilm LIKE nomFilm2 AND F.genre LIKE nomGenre AND F.sousGenre LIKE nomSousGenre AND F.annee LIKE annee2 AND F.acteurs LIKE acteur AND F.nbVotes >= nbVoteMin AND F.noteGlobale >= noteMin AND F.idFilm = V.idFilm AND V.nomUtilisateur = nomUser ORDER BY noteGlobale DESC;
 END //
 DELIMITER ;
---procédure permettant de filtrer les séries d'un utilisateur spécifique en fonction de plusieurs critères :
+--procédure permettant de filtrer les séries dun utilisateur spécifique en fonction de plusieurs critères :
 DELIMITER //
 CREATE PROCEDURE filtrerSeriesUtilisateur (IN nomSerie2 varchar(100), IN nomGenre varchar(50), IN nomSousGenre varchar(50), IN annee2 varchar(6), IN acteur varchar(100), IN saison2 varchar(10),IN nbVoteMin integer, IN noteMin double, IN nomUser varchar(50))
 BEGIN
@@ -101,14 +101,14 @@ BEGIN
 INSERT INTO genres VALUES (nomGenre);
 END //
 DELIMITER ;
---procédure permettant de voir les commentaires et notes des utilisateurs d'un film en particulier :
+--procédure permettant de voir les commentaires et notes des utilisateurs dun film en particulier :
 DELIMITER //
 CREATE PROCEDURE voirCommentairesNotesUtilisateursFilms (IN idFilm2 integer)
 BEGIN
 SELECT V.nomUtilisateur, V.note, C.contenu FROM votesFilms V, commentairesFilms C WHERE V.idFilm = idFilm2 AND V.id = C.id ;
 END //
 DELIMITER;
---procédure permettant de voir les commentaires et notes des utilisateurs d'une série en particulier :
+--procédure permettant de voir les commentaires et notes des utilisateurs dune série en particulier :
 DELIMITER //
 CREATE PROCEDURE voirCommentairesNotesUtilisateursSeries (IN idSerie2 integer)
 BEGIN
